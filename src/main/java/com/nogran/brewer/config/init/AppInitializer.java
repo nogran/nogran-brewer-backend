0941ebd0a336556898f8cef192b5b6ca9d91c815
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-07-04 20:00:48 -0300

    update

---
+++ src/main/java/com/nogran/brewer/config/init/AppInitializer.java
@@ -0,0 +1,24 @@
+package com.nogran.brewer.config.init;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.nogran.brewer.config.WebConfig;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
