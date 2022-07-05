c24cdc68a257067e55a24a235869463be37d6906
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-07-04 23:14:56 -0300

    atributos adicionados

--- src/main/java/com/nogran/brewer/model/Cerveja.java
+++ src/main/java/com/nogran/brewer/model/Cerveja.java
@@ -1,14 +1,37 @@
 package com.nogran.brewer.model;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.NotBlank;
 
 public class Cerveja {
 	
-	@NotBlank
+	@NotBlank(message = "O atributo SKU nao pode ser vazio.")
 	private String sku;
 	
+	@NotBlank(message = "O atributo Nome nao pode ser vazio.")
 	private String nome;
+	
+	@NotBlank(message = "O atributo Descricao nao pode ser vazio.")
+	@Size(min = 5, max = 255, message = "Número mínimo de caracteres é 5 e número máximo de caracteres é 255.")
+	private String descricao;
+	
+	@NotNull(message = "O atributo Quantidade nao pode ser nulo.")
+	private int quantidade;
 		
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 	public String getSku() {
 		return sku;
 	}
