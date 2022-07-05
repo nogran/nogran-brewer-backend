5bfa048ef04e0175be2f924b519dd7e4496613c0
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-07-04 23:16:33 -0300

    atributos adicionados

--- src/main/java/com/nogran/brewer/controller/CervejasController.java
+++ src/main/java/com/nogran/brewer/controller/CervejasController.java
@@ -3,9 +3,11 @@ package com.nogran.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.nogran.brewer.model.Cerveja;
 
@@ -18,13 +20,18 @@ public class CervejasController {
 	}
 
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes ) {
 		if (result.hasErrors()) {
-			System.out.println(">>> tem erro sim");
+			model.addAttribute("mensagem", "Erro no formulario!");
+			return "cerveja/CadastroCerveja";
 		}
 		
-		System.out.println(">>> sku:" + cerveja.getSku());
-		return "cerveja/CadastroCerveja";
+		System.out.println(">>> sku: " + cerveja.getSku());
+		System.out.println(">>> nome: " + cerveja.getNome());
+		System.out.println(">>> descricao: " + cerveja.getDescricao());
+		System.out.println(">>> quantidade: " + cerveja.getQuantidade());
+		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
+		return "redirect:/cervejas/novo";
 		
 	}
 }
